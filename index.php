60e57aaa9cfcde3749350c0dfc8d056706e54f6f
Siddharth Vadgama <me@siddv.net>
2016-05-10 23:41:58 +0100

    adds British Airways to portfolio, removes Alcalc

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		<article class="inner_artile">
 			<h2>Who I am</h2>
 			<p>
-				I'm Sidd; a designer, developer and general all-round maker. I like creating both virtual and physical experiences, being versatile enough to tackle any part of a project, be it design, coding or hardware. Focusing on bringing all elements together to create a streamlined user experience.
+				I'm Sidd; a designer, developer and general all-round maker. I like creating both virtual and physical experiences, being versatile enough to tackle any part of a project, be it design, development or hardware. Focusing on bringing all elements together to create a streamlined user experience.
 			</p>
 			<p>
 				Currently I'm based in London UK working as an Angular developer at <a href="http://ba.com" target="_blank">British Airways</a>, having graduated last year in BSc <a href="http://digitalartandtechnology.co.uk" target="_blank">Digital Art &amp; Technology</a> with a first-class honours from Plymouth University.
@@ -24,7 +24,6 @@
 	<section class="artile full" id="portfolio"> 
 		<article class="inner_artile">
 			<h2>Some things I've done</h2>
-			
 
 			<article class="project">
 				<img src="/resources/images/portfolio/mobius.png" />
@@ -32,9 +31,21 @@
 					<h3>Mobius</h3>
 
 					<p>
-						<a href="http://mobius.siddv.net/" target="_blank">Mobius</a> was my university final project. It is a framework for creating wireless Arduino projects with web-based control. Mobius generates both, the web-based control and the Ardunio code, allowing you to create huge porions of projects without writing a single line of code.
+						<a href="http://mobius.siddv.net/" target="_blank">Mobius</a> was my university final project. It is a framework for creating wireless &ldquo;Internet of Things&rdquo; Ardunio projects with web-based control. Mobius generates both, the web-based control and the Ardunio code, allowing you to create huge portions of projects without writing a single line of code.
 					</p>
 					<a href="/projects/mobius" class="view-more">view project</a>
+				</article>
+			</article>
+
+			<article class="project">
+				<img src="/resources/images/portfolio/british-airways.png" />
+				<article>
+					<h3>British Airways</h3>
+
+					<p>
+						The project I am working on at British Airways involves re-creating the entire booking platform from the ground up as an AngularJS application. The project aims to transform the current platform into a cleaner, easier and user-focused experience.   
+					</p>
+					<a href="javascript:void(0)" class="view-more disabled">not yet available</a>
 				</article>
 			</article>
 
@@ -95,18 +106,6 @@
 						<a href="http://termisoc.org" target="_blank">TermiSoc</a> is Plymouth University's computing society, of which I was chairman of for two-and-a-half years from 2011 to 2014. Together we ran weekly events, and more infrequent larger events, such as 50+ person LAN parties. I also was in charge of creating all branding, promotional material and other media for the society.
 					</p>
 					<a href="/projects/termisoc" class="view-more">view project</a>
-				</article>
-			</article>
-
-			<article class="project">
-				<img src="/resources/images/portfolio/alcalc.png" />
-				<article>
-					<h3>AlCalc</h3>
-
-					<p>
-						A quick and easy way to calculate which alcoholic drinks have the best value for money. Fill out the form to work out the number of units of alcohol you get per pound.
-					</p>
-					<a href="/alcalc" class="view-more">view project &nbsp;<img class="external-link-img" src="/resources/images/external-link.png"/></a>
 				</article>
 			</article>
 
