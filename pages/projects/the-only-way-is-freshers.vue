2b001420308d1148f8613a50637c358a058ef7c4
Siddharth Vadgama <me@siddv.net>
2018-07-02 22:06:11 +0100

    add towif

---
+++ pages/projects/the-only-way-is-freshers.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+
+    <page-title
+      title="The Only Way is Freshers"
+      subtitle="Student Event"/>
+
+    <section class="section">
+      <div class="container">
+        <img style="max-width: 600px;" alt="TOWIF poster" src="~/assets/images/portfolio/towif/poster.png" />
+      </div>
+    </section>
+    
+    <section class="section">
+      <div class="container content sv__copy-block">
+        <p>
+          Pictured above the poster I designed for an event hosted by Plymouth-based company, SoLarge. The event was themed around the pseudo-reality televison show The Only Way Is Essex. I was tasked to design a logo, theme and acompanying media for the event. Deadling with SoLarge, I produced the poster above, facebook banner (pictured below) and a number of assets for SoLarge to use on other media. 
+        </p> 
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <img style="max-width: 600px;" alt="TOWIF facebook" src="~/assets/images/portfolio/towif/facebook.png" />
+      </div>
+    </section>	
+  </div>
+</template>
+
+<style lang="scss" scoped>
+img {
+  display: block;
+  margin: 0 auto;
+}
+</style>
+
