5e9d959b189a5035433aee0eac23d06f2c76cdb9
Siddharth Vadgama <siddv@hotmail.com>
2018-07-18 15:25:27 +0100

    add user-set minimum roll total

--- functions/src/randchar-slack.js
+++ functions/src/randchar-slack.js
@@ -1,34 +1,62 @@
 exports.handler = async (event, context) => {
   
-  const randchar = {
-    total: 0,
-    rolls: [],
-    rollDetails: [],
-  }
-  let details = '';
+  let min = 18;
+  let error = '';
+  let rolledCharacter = {}
 
-  for(let i=0;i<=5;i++) {
-    let d6s = [];
-    let rollTotal;
-    let lowestNumber;
+  if(event && event.queryStringParameters && event.queryStringParameters.text) {
+    const userMin = parseInt(event.queryStringParameters.text)
 
-    for(let a=0;a<=3;a++) {
-      d6s.push(Math.ceil(Math.random() * 6))
-    }    
-
-    lowestNumber = d6s.sort().splice(0, 1);
-    rollTotal = d6s.reduce((a, b) => a + b, 0);
-
-    randchar.rollDetails.push({
-      total: rollTotal,
-      details: `[~${lowestNumber.join()}~, *${d6s.join('*, *')}*] = *${rollTotal}*`
-    }) 
-    randchar.rolls.push(rollTotal);
-    randchar.total += rollTotal;
+    if(!isNaN(userMin) && userMin >= 18 && userMin <= 90) {
+      min = userMin;
+    } else {
+      error = 'Please specify a number between 18 and 90 (until I figure out a better solution) e.g. `/randchar 75`'
+    }
   }
 
-  randchar.rollDetails.sort((a,b) => b.total > a.total);
-  randchar.rollDetails.forEach(val => details += `${val.details}\n`)
+  function rollChar () {
+    const randchar = {
+      total: 0,
+      rolls: [],
+      details: '',
+      rollDetails: [],
+    }
+  
+    for(let i=0;i<=5;i++) {
+      let d6s = [];
+      let rollTotal;
+      let lowestNumber;
+  
+      for(let a=0;a<=3;a++) {
+        d6s.push(Math.ceil(Math.random() * 6))
+      }    
+  
+      lowestNumber = d6s.sort().splice(0, 1);
+      rollTotal = d6s.reduce((a, b) => a + b, 0);
+  
+      randchar.rollDetails.push({
+        total: rollTotal,
+        details: `[~${lowestNumber.join()}~, *${d6s.join('*, *')}*] = *${rollTotal}*`
+      }) 
+      randchar.rolls.push(rollTotal);
+      randchar.total += rollTotal;
+    }
+  
+    randchar.rollDetails.sort((a,b) => b.total > a.total);
+    randchar.rollDetails.forEach(val => randchar.details += `${val.details}\n`)
+
+    return randchar;
+  }
+  
+  if(!error.length) {
+    
+    rolledCharacter = rollChar();
+  
+    while(rolledCharacter.total <= min){
+      rolledCharacter = rollChar();
+    }
+
+  }
 
   return {
     headers: {
@@ -37,10 +65,13 @@ exports.handler = async (event, context) => {
     statusCode: 200,
     body: JSON.stringify({
       response_type: "in_channel",
-      text: `Total: *${randchar.total}*\nRolls: *${randchar.rolls.sort((a,b) => b>a).join('*, *')}*`,
+      text: error.length ? '' : `Total: *${rolledCharacter.total}*\nRolls: *${rolledCharacter.rolls.sort((a,b) => b>a).join('*, *')}*`,
       attachments: [
         {
-          text: `${details}`
+          text: error.length ? '' : `${rolledCharacter.details}`
+        },
+        {
+          text: error
         }
       ]
     })
