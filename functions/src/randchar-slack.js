d7b467c165936470a2173b59dfc93a718951e4a1
Siddharth Vadgama <siddv@hotmail.com>
2018-07-18 12:34:10 +0100

    add roll details to randchar function

--- functions/src/randchar-slack.js
+++ functions/src/randchar-slack.js
@@ -1,22 +1,24 @@
 exports.handler = async (event, context) => {
-
-  console.log('hello')
   
   const randchar = {
     total: 0,
-    rolls: []
+    rolls: [],
+    rollDetails: [],
   }
 
   for(let i=0;i<=5;i++) {
     let d6s = [];
     let rollTotal;
+    let lowestNumber;
 
     for(let a=0;a<=3;a++) {
       d6s.push(Math.ceil(Math.random() * 6))
     }    
 
+    lowestNumber = d6s.sort().splice(0, 1);
     rollTotal = d6s.reduce((a, b) => a + b, 0);
-    d6s.sort().splice(0, 1);
+
+    randchar.rollDetails.push(`*${rollTotal}*: [*${d6s.join('*, *')}* ${lowestNumber.join()}]`) 
     randchar.rolls.push(rollTotal);
     randchar.total += rollTotal;
   }
@@ -28,7 +30,12 @@ exports.handler = async (event, context) => {
     statusCode: 200,
     body: JSON.stringify({
       response_type: "in_channel",
-      text: `Total: *${randchar.total}*\nRolls: *${randchar.rolls.join('*, *')}*`
+      text: `Total: *${randchar.total}*\nRolls: *${randchar.rolls.join('*, *')}*`,
+      attachments: [
+        {
+          text: `${randchar.rollDetails.join('\n')}`
+        }
+      ]
     })
   };
 
