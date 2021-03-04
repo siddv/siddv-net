f8d5a6121277cb768ddc9472d57ecf3453480d47
Siddharth Vadgama <me@siddv.net>
2021-03-04 11:45:50 +0000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -11,10 +11,10 @@
         <blockquote class="container has-text-centered blockquote sv__hero-blurb">
           <h2 class="title is-4">Who I am</h2>
           <p>
-            I'm Sidd; a creative technologist. I like creating both virtual and physical experiences, being versatile enough to tackle large and diverse parts of projects. Focusing on bringing all elements together to create a streamlined user experience.
+            I'm Sidd; a technologist. I like creating both virtual and physical experiences, being versatile enough to tackle large and diverse parts of projects. Focusing on bringing all elements together to create a streamlined user experience.
           </p>
           <p>
-            Currently, I'm based in London working as a Senior Frontend Developer at <a class="sv__light-link" href="http://idean.com/" target="_blank" rel="noopener">Idean&nbsp;UK</a>. Previously at <a class="sv__light-link" href="http://www.tribalworldwide.co.uk/" target="_blank"  rel="noopener">Tribal&nbsp;Worldwide</a>, and <a class="sv__light-link" href="http://www.ba.com/" target="_blank">British&nbsp;Airways</a>.
+            Currently, I'm based in London working as a Lead Developer at <a class="sv__light-link" href="http://idean.com/" target="_blank" rel="noopener">Idean&nbsp;UK</a>. Previously at <a class="sv__light-link" href="http://www.tribalworldwide.co.uk/" target="_blank"  rel="noopener">Tribal&nbsp;Worldwide</a>, and <a class="sv__light-link" href="http://www.ba.com/" target="_blank">British&nbsp;Airways</a>.
           </p>
         </blockquote>
       </div>
