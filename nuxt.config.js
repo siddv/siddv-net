38a60fa1a3d4bf65b5f9d982796d360c0a0274fd
Siddharth Vadgama <me@siddv.net>
2025-09-25 12:10:45 +0100

    Update metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s | Siddv.net',
+    titleTemplate: '%s | siddv.net',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'The personal website of Siddharth Vadgama; A creative technologist based in London, UK.' }
+      { hid: 'description', name: 'description', content: 'The personal website of Siddharth Vadgama; A creative technologist based in Bristol, UK.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
